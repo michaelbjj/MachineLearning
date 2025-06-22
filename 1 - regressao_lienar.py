39268430955f21667dd658bdb675b79dc81da274
GAMER <michaelissao923@gmail.com>
2025-06-22 14:34:18 -0300

    Aula 3

--- 1 - regressao_lienar.py
+++ 1 - regressao_lienar.py
@@ -1,5 +1,7 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 frases =  [
       "Estou feliz hoje!",
@@ -25,3 +27,14 @@ print("Palavras", vectorizer.get_feature_names_out())
 print(X)
 
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+EmotionIA = LogisticRegression()
+
+#Treinando Modelo
+EmotionIA.fit(x_train, y_train)
+
+y_pred =  EmotionIA.predict(x_test)
+
+acurity = accuracy_score(y_test, y_pred)
+
+print("Acurácia Modelo", acurity)
