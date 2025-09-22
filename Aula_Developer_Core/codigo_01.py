bf565544c0875e39ec893b901476a8ff7d1c436c
GAMER <michaelissao923@gmail.com>
2025-09-21 21:16:01 -0300

    My first code

---
+++ Aula_Developer_Core/codigo_01.py
@@ -0,0 +1,8 @@
+#Aqui estou adicionando uma váriavel do tipo nome
+
+nome = 'Michael Issao Yamaguishi'
+
+print(f'Olá,  {nome}! Bem vindo ao python')
+
+#Crie uma váriavel meu nome
+#Apresente a váriavel com print com print e rode o codigo pelo Terminal
