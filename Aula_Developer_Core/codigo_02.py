811b48a6391870b1e2069a0e27cc05922c458342
GAMER <michaelissao923@gmail.com>
2025-09-24 19:14:39 -0300

    My sec code

---
+++ Aula_Developer_Core/codigo_02.py
@@ -0,0 +1,10 @@
+numero_01  = 10
+numero_02 = 5
+
+soma   = (numero_01 + numero_02)
+subt   = (numero_01 - numero_02)
+div    = (numero_01 / numero_02)
+modulo = (numero_01 % numero_02)
+exp    = (numero_01 ** numero_02)
+
+print("Resultado", soma, subt, div, modulo, exp)
