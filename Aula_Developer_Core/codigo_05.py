70bb09ad9bdd17378585d7b82e704501eb6fcf76
GAMER <michaelissao923@gmail.com>
2025-09-30 20:17:40 -0300

    My sec code

---
+++ Aula_Developer_Core/codigo_05.py
@@ -0,0 +1,8 @@
+#Interação com Usuário
+nome      = input('Qual é o seu nome? ')
+idade     = input('Qual é o sua idade? ')
+profissao = input('Qual sua profissão? ')
+
+print(f'Olá, {nome}')
+print(f'Idade, {idade}')
+print(f'Profissão, {profissao}')
