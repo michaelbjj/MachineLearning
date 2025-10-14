a397ebc3b883af35b3287ed450c45b0361db763c
GAMER <michaelissao923@gmail.com>
2025-10-14 18:37:19 -0300

    My sec code

---
+++ Aula_Developer_Core/codigo_10.py
@@ -0,0 +1,10 @@
+#def quadrado(numero):
+#    return numero ** 2
+
+#resultado = quadrado(4)
+
+#print(f'O quadrado é : {resultado} ')
+
+def mensagem():
+    print('Essa é a função sem return')
+mensagem()
