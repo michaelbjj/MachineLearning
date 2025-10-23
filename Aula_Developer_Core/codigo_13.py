ee1b473b1de1a0f88672984b9ee62c83253d721a
GAMER <michaelissao923@gmail.com>
2025-10-23 18:50:39 -0300

    My sec code

---
+++ Aula_Developer_Core/codigo_13.py
@@ -0,0 +1,20 @@
+# Calculdora cm Funções
+def Calculadora(a, b, operacao):
+    
+    if operacao == 'Soma':
+        return a + b
+    elif operacao == 'Substr':
+        return a - b
+    elif operacao == 'Multi':
+        return a * b
+    elif operacao == 'Divi':
+        return a / b
+    else:
+        return "Operação Inválida"
+    
+    print(Calculadora(10, 2, 'Soma'))
+# Gerador de Senhas
+
+# Conversor de Temperatura
+
+# Calcular Imposto
