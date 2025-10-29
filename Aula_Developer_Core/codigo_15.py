2c5607b005069a7a4a326897d42025777630c52f
GAMER <michaelissao923@gmail.com>
2025-10-29 20:35:31 -0300

    My sec code

---
+++ Aula_Developer_Core/codigo_15.py
@@ -0,0 +1,2 @@
+def celsius_for_fahrenheit(c):
+    return c * 9 / 5 + 32
--- Aula_Developer_Core/funcoes.py
+++ Aula_Developer_Core/funcoes.py
@@ -1,3 +1,12 @@
+#codigo_12
 import codigo_12
 
 print(codigo_12.saudacao("Michael"))
+
+#celsius_for_fahrenheit
+from codigo_15 import celsius_for_fahrenheit
+
+c1 = celsius_for_fahrenheit(25)
+
+print(f'Resultado celsius_for_fahrenheit {c1}')
+
