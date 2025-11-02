3dcdf887bdc87880b09b738fa3256c0cb7caec26
GAMER <michaelissao923@gmail.com>
2025-11-02 20:09:16 -0300

    My sec code

---
+++ Aula_Developer_Core/listas.py
@@ -0,0 +1,19 @@
+#frutas = ['maça', 'banana', 'laranja']
+#print(frutas)
+#print(frutas[0])
+#print(type(frutas))
+
+#lista = ["carro",  True, 2, 3.5]
+#print(lista)
+#print(lista[2])
+#print(type(lista[2]))
+
+#Tupla
+
+tupla = ("carro", True, 2, 3.5)
+print(tupla)
+print(type(tupla))
+print(tupla[3])
+
+
+
