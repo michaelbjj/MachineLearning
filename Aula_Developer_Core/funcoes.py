75de96f705f377d7e6c284f7920f369e4d492034
GAMER <michaelissao923@gmail.com>
2025-11-02 20:09:47 -0300

    My sec code

--- Aula_Developer_Core/funcoes.py
+++ Aula_Developer_Core/funcoes.py
@@ -1,12 +1,30 @@
 #codigo_12
-import codigo_12
+#import codigo_12
 
-print(codigo_12.saudacao("Michael"))
+#print(codigo_12.saudacao("Michael"))
 
 #celsius_for_fahrenheit
-from codigo_15 import celsius_for_fahrenheit
+#from codigo_15 import celsius_for_fahrenheit
 
-c1 = celsius_for_fahrenheit(25)
+#c1 = celsius_for_fahrenheit(25)
 
-print(f'Resultado celsius_for_fahrenheit {c1}')
+#print(f'Resultado celsius_for_fahrenheit {c1}')
+
+def calcular_imposto(valor):
+    if valor < 1000:
+        imposto = valor * 0.1
+    elif valor < 2000:
+        imposto = valor * 0.13
+    else:
+        imposto = valor * 0.2
+    return imposto
+
+preco_produto1 = input('Digite o valor do produto 01: ')
+preco_produto2 = input('Digite o valor do produto 02: ')
+
+imposto_produto1 = calcular_imposto(int(preco_produto1))
+imposto_produto2 = calcular_imposto(int(preco_produto2))
+
+print(imposto_produto1)
+print(imposto_produto2)
 
