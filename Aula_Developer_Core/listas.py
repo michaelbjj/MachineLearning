4876377af63960b9fc1041c9b293e012aa1091da
GAMER <michaelissao923@gmail.com>
2025-11-05 19:26:10 -0300

    My sec code

--- Aula_Developer_Core/listas.py
+++ Aula_Developer_Core/listas.py
@@ -10,10 +10,25 @@
 
 #Tupla
 
-tupla = ("carro", True, 2, 3.5)
-print(tupla)
-print(type(tupla))
-print(tupla[3])
+#tupla = ("carro", True, 2, 3.5)
+#print(tupla)
+#print(type(tupla))
+#print(tupla[3])
 
+#dicionario = {"nome": "Asaph", "logica" : True, "numero": 2, "outro_numero": 3.5}
 
+#print(dicionario)
+#print(type(dicionario))
+#print(dicionario["logica"])
 
+pessoa = {"nome": "Asaph", "idade": 20, "cidade": "Curitiba"}
+print(pessoa["cidade"])
+
+print(pessoa.keys())
+
+print(pessoa.values())
+
+print(pessoa.items())
+
+for chave, valor in pessoa.items():
+    print(f"{chave}: {valor}")
