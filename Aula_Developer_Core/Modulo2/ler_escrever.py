eb86b6700e109a076211a22c5e64a4bc71ead584
GAMER <michaelissao923@gmail.com>
2025-11-17 19:14:02 -0300

    Movi arquivos para nova estrutura de pastas

--- Aula_Developer_Core/Modulo2/ler_escrever.py
+++ Aula_Developer_Core/Modulo2/ler_escrever.py
@@ -7,6 +7,13 @@
 
 #arquivo.close()
 
-with open(r"C:\Projetos\Projetos ItValley\Aula_Developer_Core\Modulo2\exemplo.txt", 'r') as arquivo:
-    conteudo = arquivo.read()
-    print(conteudo)
+#with open(r"C:\Projetos\Projetos ItValley\Aula_Developer_Core\Modulo2\exemplo.txt", 'r') as arquivo:
+#    conteudo = arquivo.read()
+#    print(conteudo)
+
+with open(r"C:\Projetos\Projetos ItValley\Aula_Developer_Core\Modulo2\exemplo.txt", 'r') as arquivo :
+    print(arquivo.read())    
+    arquivo.seek(0)
+    print(arquivo.readline())
+    arquivo.seek(0)
+    print(arquivo.readlines())
