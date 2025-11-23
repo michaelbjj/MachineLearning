1bde61990629c7c92c6d0d1c80962b7567f6dcd3
GAMER <michaelissao923@gmail.com>
2025-11-23 18:02:27 -0300

    Atualizando..

--- Aula_Developer_Core/Modulo2/ler_escrever.py
+++ Aula_Developer_Core/Modulo2/ler_escrever.py
@@ -11,9 +11,27 @@
 #    conteudo = arquivo.read()
 #    print(conteudo)
 
-with open(r"C:\Projetos\Projetos ItValley\Aula_Developer_Core\Modulo2\exemplo.txt", 'r') as arquivo :
-    print(arquivo.read())    
-    arquivo.seek(0)
-    print(arquivo.readline())
-    arquivo.seek(0)
-    print(arquivo.readlines())
+#with open(r"C:\Projetos\Projetos ItValley\Aula_Developer_Core\Modulo2\exemplo.txt", 'r') as arquivo :
+#    print(arquivo.read())    
+#    arquivo.seek(0)
+#    print(arquivo.readline())
+#    arquivo.seek(0)
+#    print(arquivo.readlines())
+
+#sArquivo = r'C:\Projetos\Projetos ItValley\Aula_Developer_Core\Modulo2\exemplo.txt'
+
+#with open (sArquivo, 'w') as arquivo:
+#    arquivo.write("Primeira Linha\n")
+#    arquivo.writelines(["Segunda linha\n", "Terceira linha\n"])
+
+import csv
+
+with open("dados.csv", "w", newline="") as arquivo:
+    escritor = csv.writer(arquivo)
+    escritor.writerow(["Nome", "Idade", "Cidade"])
+    escritor.writerow(["Michael", 43, "Curitiba"])
+
+with open("dados.csv", "r") as arquivo :
+    leitor = csv.reader(arquivo)
+    for linha in leitor:
+        print(linha)
